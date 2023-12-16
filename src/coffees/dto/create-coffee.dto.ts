bed3c722a351f3ddfd91ee03852926ed2cea8e93
tushartak <tushartak861@gmail.com>
2023-12-16 23:46:29 +0530

    Validating the DTO's

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,6 +1,15 @@
+import { IsString, IsNumber } from "class-validator";
+
 export class CreateCoffeeDto {
+    @IsNumber()
     readonly id:number;
+    @IsString()
     readonly name: string;
+    @IsString()
     readonly brand: string;
+    @IsString({ each: true })
     readonly flavors: string[];
 }
+
+// DTO's are usefull in creating a bit of type safety within our web applications.
+// they let us create a definition for the shape od the data that's coming into the body of the API Request. 
--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,5 +1,7 @@
-export class UpdateCoffeeDto {
-    readonly name?: string;
-    readonly brand?: string;
-    readonly flavors?: string[];
-}
+import {PartialType} from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
+
+
+// PartialType utility function helps to reduce redundant code.
+// here it inherits all the fields from createCoffeeDto with its validation decorators + add an IsOptional() decorator to all.
