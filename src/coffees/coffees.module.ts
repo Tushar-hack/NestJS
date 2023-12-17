13b7975f4126dd89707c11f4044c8ee91166e7dd
tushartak <tushartak861@gmail.com>
2023-12-17 18:05:23 +0530

    Added Configuration and Entity in Coffee Module

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-    imports: [],
+    imports: [TypeOrmModule.forFeature([Coffee])],
     controllers: [CoffeesController],
     providers: [CoffeesService],
     exports: [CoffeesModule]
