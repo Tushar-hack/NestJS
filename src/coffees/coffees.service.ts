f10fa697f28273ce95158f09a39aab2522f0403f
tushartak <tushartak861@gmail.com>
2023-12-19 00:35:24 +0530

    Transactions using Query Runner

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -3,9 +3,10 @@ import { Coffee } from './entities/coffee.entity';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { Flavor } from './entities/flavor.entity';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
+import { Event } from 'src/events/entities/event.entity/event.entity';
 
 @Injectable()
 export class CoffeesService {
@@ -22,7 +23,8 @@ export class CoffeesService {
         @InjectRepository(Coffee) 
         private readonly coffeeRepository: Repository<Coffee>,
         @InjectRepository(Flavor)
-        private readonly flavorRepository: Repository<Flavor>
+        private readonly flavorRepository: Repository<Flavor>,
+        private readonly connection: Connection,
     ) {}
 
     findAll(paginationQuery: PaginationQueryDto) {
@@ -92,5 +94,32 @@ export class CoffeesService {
         }
 
         return this.flavorRepository.create({name});
+    }
+
+    async recommendCoffee(coffee: Coffee) {
+        const queryRunner = this.connection.createQueryRunner();
+
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+
+        try {
+            coffee.recomendations++;
+
+            const recommendEvent = new Event();
+            recommendEvent.name = 'recommend_coffee';
+            recommendEvent.type = 'coffee';
+            recommendEvent.payload = {
+                coffeeId: coffee.id
+            }
+
+            await queryRunner.manager.save(coffee);
+            await queryRunner.manager.save(recommendEvent);
+
+            await queryRunner.commitTransaction();
+        } catch (error) {
+            await queryRunner.rollbackTransaction();
+        }finally{
+            await queryRunner.release();
+        }
     }
 }
